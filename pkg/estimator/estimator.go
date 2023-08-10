8b802987883c6a5a78772ca469e091e84e917ce0
Petr Ivanov <petr.ivanov@tendermint.com>
2023-08-10 16:30:08 +0400

    optimize memory usage

--- pkg/estimator/estimator.go
+++ pkg/estimator/estimator.go
@@ -4,13 +4,20 @@ import (
 	"math/big"
 )
 
+var (
+	comissionK = big.NewInt(997)
+	grand      = big.NewInt(1000)
+)
+
 // Estimate estimates output of uniswap v2 pair swap
 func Estimate(inputAmount, inputReserve, outputReserve *big.Int) *big.Int {
-	var inputWithFee, numerator, denominator, result = &big.Int{}, &big.Int{}, &big.Int{}, &big.Int{}
-	inputWithFee.Mul(inputAmount, big.NewInt(997))
-	numerator.Mul(inputWithFee, outputReserve)
-	denominator.Mul(inputReserve, big.NewInt(1000))
-	denominator.Add(denominator, inputWithFee)
-	result.Div(numerator, denominator)
-	return result
+	inputAmount.Mul(inputAmount, comissionK)
+	// reuse outputReserve as numerator to avoid allocations
+	outputReserve.Mul(inputAmount, outputReserve)
+	// reuse inputReserve as denominator to avoid extra allocations
+	inputReserve.Mul(inputReserve, grand)
+	inputReserve.Add(inputReserve, inputAmount)
+	// reuse inputAmount as result to avoid extra allocations
+	// numerator(outputReserve) / denominator()
+	return inputAmount.Div(outputReserve, inputReserve)
 }
